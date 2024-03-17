7249ba9c1ef44cb4379a988a3e4c8a18952c9ee4
Evgeny Koshelev <eko2000@gmail.com>
2024-03-17 14:20:27 +0100

    Fix the contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,16 +3,18 @@
 */
 
 require('dotenv').config();
-import "@nomiclabs/hardhat-ethers";
+require("@nomiclabs/hardhat-ethers");
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
-export const solidity = "0.8.7";
-export const defaultNetwork = "goerli";
-export const networks = {
-  hardhat: {},
-  goerli: {
-    url: API_URL,
-    accounts: [`0x${PRIVATE_KEY}`]
-  }
-};
+module.exports = {
+  solidity: "0.8.20",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+}
