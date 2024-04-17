1bf19a43c7baa44b5bfb35ce46bd8805947445f0
Evgeny Koshelev <eko2000@gmail.com>
2024-04-17 23:30:18 +0200

    Fix shart contract import directives

--- contracts/contract-fb4e48dd3a.sol
+++ contracts/contract-fb4e48dd3a.sol
@@ -2,8 +2,8 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract CleanAppKITN is ERC20, ERC20Permit {
     constructor() ERC20("CleanApp KITN", "KITN") ERC20Permit("CleanApp KITN") {
