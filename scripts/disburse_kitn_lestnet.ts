c2ed86983961d2f3890a14761347b4191de62160
Evgeny Koshelev <eko2000@gmail.com>
2024-10-24 14:45:33 +0200

    Update lestnet tokens

--- scripts/disburse_kitn_lestnet.ts
+++ scripts/disburse_kitn_lestnet.ts
@@ -3,7 +3,7 @@ import { ethers } from 'hardhat';
 async function main() {
   const kitn = await ethers.deployContract(
     'KitnDisbursement',
-    ['0x7ccD117FC62B90C14a60eD821f55470B76cf7fb7']
+    ['0x2D29ee5D409e66482EB5C4FBCaF092CeC4e57A8c']
   );
   await kitn.waitForDeployment();
   console.log("KitnDisbursement contract deployed to address:", kitn.target);
